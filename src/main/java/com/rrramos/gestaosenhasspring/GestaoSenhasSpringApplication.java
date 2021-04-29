d8919ea24a998a4129150a20ccd7ce9416a55c3f
RRRAMOS <dsraquelramos@gmail.com>
2021-04-29 15:05:38 -0300

    estrutura inicial

---
+++ src/main/java/com/rrramos/gestaosenhasspring/GestaoSenhasSpringApplication.java
@@ -0,0 +1,13 @@
+package com.rrramos.gestaosenhasspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoSenhasSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoSenhasSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rrramos/gestaosenhasspring/GestaoSenhasSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rrramos.gestaosenhasspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoSenhasSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
