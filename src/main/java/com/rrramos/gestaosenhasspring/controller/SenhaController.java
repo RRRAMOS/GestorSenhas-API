1ad7eea39bae41615d1ec766d35c27a5f2e42dfe
RRRAMOS <dsraquelramos@gmail.com>
2021-04-29 17:28:51 -0300

    correçaõ de return

--- src/main/java/com/rrramos/gestaosenhasspring/controller/SenhaController.java
+++ src/main/java/com/rrramos/gestaosenhasspring/controller/SenhaController.java
@@ -1,2 +1,32 @@
-package com.rrramos.gestaosenhasspring.controller;public class SenhaController {
+package com.rrramos.gestaosenhasspring.controller;
+
+import com.rrramos.gestaosenhasspring.servico.SenhaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class SenhaController {
+
+    @Autowired
+    private SenhaController senhaService;
+
+    @PostMapping("/nova/Senha/Normal")
+    public String novaSenha(){
+        return senhaService.novaSenha();
+    }
+
+    @PostMapping("/nova/Senha/Preferencial")
+    public String novaSenhaPreferencial(){
+        return senhaService.novaSenhaPreferencial();
+    }
+
+    @GetMapping("senha/proxima")
+    public List<Senha> findAllSenhas{
+        return senhaService.findAllSenhas();
+    }
+
+
 }
